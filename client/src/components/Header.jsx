4d9f55329a4357739d20cae170322fad0c68abef
jgafron <jgafron@pdx.edu>
2024-06-05 02:20:58 -0700

    Added profile pictureto conversations, added profile picture to navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Nav, Navbar, NavDropdown, Badge } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { FaSignInAlt, FaUserPlus } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -8,76 +8,95 @@ import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 
 function Header() {
-	const { userInfo } = useSelector((state) => state.auth);
+    const { userInfo } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [logoutApiCall] = useLogoutMutation();
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+    console.log(userInfo); // Add this line to check the contents of userInfo
 
-	const [logoutApiCall] = useLogoutMutation();
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate("/");
+        } catch (error) {
+            console.error("Failed to logout: ", error);
+        }
+    };
 
-	const logoutHandler = async () => {
-		try {
-			await logoutApiCall().unwrap();
-			dispatch(logout());
-			navigate("/");
-		} catch (error) {
-			console.error("Failed to logout: ", error);
-		}
-	};
-
-	return (
-		<header>
-			<Navbar data-bs-theme="light">
-				<Container>
-					<Navbar.Brand as={Link} to="/">
-						GamerChat
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="me-auto">
-							<Nav.Link as={Link} to="/">
-								Home
-							</Nav.Link>
-							<Nav.Link as={Link} to="/chat">
-								Chat
-							</Nav.Link>
-							<Nav.Link as={Link} to="/groupchat">
-								Group Chat
-							</Nav.Link>
-						</Nav>
-						<Nav className="ms-auto">
-							{userInfo ? (
-								<>
-									<NavDropdown
-										title={userInfo.displayName}
-										id="displayname">
-										<NavDropdown.Item
-											as={Link}
-											to="/profile">
-											Profile
-										</NavDropdown.Item>
-										<NavDropdown.Item
-											onClick={logoutHandler}>
-											Logout
-										</NavDropdown.Item>
-									</NavDropdown>
-								</>
-							) : (
-								<>
-									<Nav.Link as={Link} to="/login">
-										<FaSignInAlt /> Log In
-									</Nav.Link>
-									<Nav.Link as={Link} to="/register">
-										<FaUserPlus /> Sign Up
-									</Nav.Link>
-								</>
-							)}
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</header>
-	);
+    return (
+        <header>
+            <Navbar data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        GamerChat
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/chat">
+                                Chat
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/groupchat">
+                                Group Chat
+                            </Nav.Link>
+                        </Nav>
+                        <Nav className="ms-auto">
+                            {userInfo ? (
+                                <>
+                                    <NavDropdown
+                                        title={
+                                            <>
+                                                {userInfo.profilePicture && (
+                                                    <img
+                                                        src={`data:${userInfo.profilePicture.contentType};base64,${userInfo.profilePicture.data}`}
+                                                        alt="Profile"
+                                                        className="rounded-circle"
+                                                        style={{
+                                                            width: "30px",
+                                                            height: "30px",
+                                                            marginRight: "10px"
+                                                        }}
+                                                    />
+                                                )}
+                                                {userInfo.displayName}
+                                            </>
+                                        }
+                                        id="displayname"
+                                    >
+                                        <NavDropdown.Item
+                                            as={Link}
+                                            to="/profile"
+                                        >
+                                            Profile
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Item
+                                            onClick={logoutHandler}
+                                        >
+                                            Logout
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </>
+                            ) : (
+                                <>
+                                    <Nav.Link as={Link} to="/login">
+                                        <FaSignInAlt /> Log In
+                                    </Nav.Link>
+                                    <Nav.Link as={Link} to="/register">
+                                        <FaUserPlus /> Sign Up
+                                    </Nav.Link>
+                                </>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
 }
 
 export default Header;
--- client/src/slices/authSlice.js
+++ client/src/slices/authSlice.js
@@ -4,27 +4,27 @@ import Cookies from "js-cookie";
 // authentication functions for the frontend
 
 const initialState = {
-	userInfo: localStorage.getItem("userInfo")
-		? JSON.parse(localStorage.getItem("userInfo"))
-		: null,
-	token: localStorage.getItem("token")
+    userInfo: localStorage.getItem("userInfo")
+        ? JSON.parse(localStorage.getItem("userInfo"))
+        : null,
+    token: localStorage.getItem("token")
 };
 
 const authSlice = createSlice({
-	name: "auth",
-	initialState,
-	reducers: {
-		setCredentials: (state, action) => {
-			state.userInfo = action.payload;
-			localStorage.setItem("userInfo", JSON.stringify(action.payload));
-			localStorage.setItem("token", action.payload.token);
-		},
-		logout: (state) => {
-			state.userInfo = null;
-			localStorage.removeItem("userInfo");
-			localStorage.removeItem("token");
-		}
-	}
+    name: "auth",
+    initialState,
+    reducers: {
+        setCredentials: (state, action) => {
+            state.userInfo = action.payload;
+            localStorage.setItem("userInfo", JSON.stringify(action.payload));
+            localStorage.setItem("token", action.payload.token);
+        },
+        logout: (state) => {
+            state.userInfo = null;
+            localStorage.removeItem("userInfo");
+            localStorage.removeItem("token");
+        }
+    }
 });
 
 export const { setCredentials, logout } = authSlice.actions;
