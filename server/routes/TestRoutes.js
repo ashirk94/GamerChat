f3355be714b83878de65c4e0ea2a1c02f393381f
Alan Shirk <ashirk94@gmail.com>
2024-06-09 14:29:47 -0700

    Testing updates

--- server/routes/TestRoutes.js
+++ server/routes/TestRoutes.js
@@ -10,10 +10,9 @@ router.post("/login", (req, res) => {
 	res.status(401).send("Unauthorized");
 });
 
-// change this to something besides comment
-router.post("/comment", (req, res) => {
-	const { comment } = req.body;
-	if (comment.includes("<script>")) {
+router.post("/api/messages", (req, res) => {
+	const { message } = req.body;
+	if (message.includes("<script>")) {
 		return res.status(400).send("Bad Request");
 	}
 	res.status(200).send("Comment received");
